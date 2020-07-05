2b14aae6ed176f8b542af47e435639d2c91ff3b2
Chester <chesterdeveloper2000@gmail.com>
2020-07-05 19:44:35 +0800

    adding a follow up request when a user clicks on one of the movies

--- app.js
+++ app.js
@@ -46,6 +46,11 @@ const onInput = async event => {
             <img src="${imgSrc}" width="36"/>
             ${movie.Title}
         `
+        option.addEventListener("click", () => {
+            dropdown.classList.remove("is-active")
+            input.value = movie.Title
+            onMovieSelect(movie)
+        })
         resultsWrapper.appendChild(option)
     }
 }
@@ -57,3 +62,14 @@ document.addEventListener("click", event => {
         dropdown.classList.remove("is-active")
     }
 })
+
+
+const onMovieSelect = async movie => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "ab2cde52",
+            i: movie.imdbID
+        }
+    })
+    console.log(response.data) 
+}
