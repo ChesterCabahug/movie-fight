85e9f3277f764c214b5552f13e922d68293310a9
Chester <chesterdeveloper2000@gmail.com>
2020-07-06 12:16:23 +0800

    ✨refactor: removing keyword movie in autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,12 @@
-const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) => {
+const createAutoComplete = ({
+    root,
+    renderOption,
+    onOptionSelect,
+    inputValue,
+    fetchData
+}) => {
     root.innerHTML = `
-    <label><strong>Search for a movie</strong></label>
+    <label><strong>Search</strong></label>
     <input class="input" />
     <div class="dropdown">
         <div class="dropdown-menu">
@@ -13,9 +19,9 @@ const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) 
     const resultsWrapper = root.querySelector(".results")
 
     const onInput = async event => {
-        const movies = await fetchData(event.target.value)
+        const items = await fetchData(event.target.value)
 
-        if (!movies.length) {
+        if (!items.length) {
             dropdown.classList.remove("is-active")
             return
         }
@@ -23,14 +29,14 @@ const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) 
         resultsWrapper.innerHTML = ""
         dropdown.classList.add("is-active")
 
-        for (let movie of movies) {
+        for (let item of items) {
             const option = document.createElement("a")
             option.classList.add("dropdown-item")
-            option.innerHTML = renderOption(movie)
+            option.innerHTML = renderOption(item)
             option.addEventListener("click", () => {
                 dropdown.classList.remove("is-active")
-                input.value = inputValue(movie)
-                onOptionSelect(movie)
+                input.value = inputValue(item)
+                onOptionSelect(item)
             })
             resultsWrapper.appendChild(option)
         }
