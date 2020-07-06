bff085578f2134496de1aeed3821287eff0ba253
Chester <chesterdeveloper2000@gmail.com>
2020-07-06 17:07:13 +0800

    added logic about when to compare both movies

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ createAutoComplete({
     root: document.querySelector("#left-autocomplete"),
     onOptionSelect(movie) {
         document.querySelector(".tutorial").classList.add("is-hidden")
-        onMovieSelect(movie, document.querySelector("#left-summary"))
+        onMovieSelect(movie, document.querySelector("#left-summary"), "left")
     }
 })
 createAutoComplete({
@@ -37,21 +37,40 @@ createAutoComplete({
     root: document.querySelector("#right-autocomplete"),
     onOptionSelect(movie) {
         document.querySelector(".tutorial").classList.add("is-hidden")
-        onMovieSelect(movie, document.querySelector("#right-summary"))
+        onMovieSelect(movie, document.querySelector("#right-summary"), "right")
     }
 })
 
 
+let leftMovie
+let rightMovie
 
-const onMovieSelect = async (movie, summaryElement) => {
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get("http://www.omdbapi.com/", {
         params: {
             apikey: "ab2cde52",
             i: movie.imdbID
         }
     })
+
+
+
     summaryElement.innerHTML = movieTemplate(response.data)
 
+    if (side === "left") {
+        leftMovie = response.data
+    } else {
+        rightMovie = response.data
+    }
+
+    if (leftMovie && rightMovie) {
+        runComparison()
+    }
+
+}
+
+const runComparison = () => {
+    console.log("time for comparison!")
 }
 
 const movieTemplate = (movieDetail) => {
