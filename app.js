5ff71704157509847443a035a2ba631091ef8dfc
Chester <chesterdeveloper2000@gmail.com>
2020-07-06 11:31:52 +0800

    ✨refactor: displaying multiple autocompletes

--- app.js
+++ app.js
@@ -13,55 +13,10 @@ const fetchData = async (searchTerm) =>  {
     return(response.data.Search)
 }
 
-const root = document.querySelector(".autocomplete")
-root.innerHTML = `
-    <label><strong>Search for a movie</strong></label>
-    <input class="input" />
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`
-const input = document.querySelector("input")
-const dropdown = document.querySelector(".dropdown")
-const resultsWrapper = document.querySelector(".results")
+createAutoComplete({root: document.querySelector(".autocomplete")})
+createAutoComplete({root: document.querySelector(".autocomplete2")})
+createAutoComplete({root: document.querySelector(".autocomplete3")})
 
-const onInput = async event => {
-    const movies = await fetchData(event.target.value)
-
-    if(!movies.length){
-        dropdown.classList.remove("is-active")
-        return
-    }
-
-    resultsWrapper.innerHTML = ""
-    dropdown.classList.add("is-active")
-    
-    for(let movie of movies) {
-        const option = document.createElement("a")
-        const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster
-        option.classList.add("dropdown-item")
-        option.innerHTML = `
-            <img src="${imgSrc}" width="36"/>
-            ${movie.Title}
-        `
-        option.addEventListener("click", () => {
-            dropdown.classList.remove("is-active")
-            input.value = movie.Title
-            onMovieSelect(movie)
-        })
-        resultsWrapper.appendChild(option)
-    }
-}
-
-input.addEventListener("input", debounce(onInput, 500))
-
-document.addEventListener("click", event => {
-    if(!root.contains(event.target)){
-        dropdown.classList.remove("is-active")
-    }
-})
 
 
 const onMovieSelect = async movie => {
